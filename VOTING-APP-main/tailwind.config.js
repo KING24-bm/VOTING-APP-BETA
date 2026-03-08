bf90349b6c87fe29025fb8579bcd516b5508e87a
KING24-bm <naitik.euro@gmail.com>
2026-03-08 20:02:26 +0530

    Co-authored-by: Nitika-2014 <Nitika-2014@users.noreply.github.com>

--- VOTING-APP-main/tailwind.config.js
+++ VOTING-APP-main/tailwind.config.js
@@ -3,11 +3,7 @@ export default {
   darkMode: 'class', // use class strategy so we can toggle theme manually
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      animation: {
-        "spin-slow": "spin 40s linear infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
